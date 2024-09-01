5a24762432392eb9639f64160b9311905c43bfdd
drewmarsh <drewmmisc@gmail.com>
2024-09-01 14:47:42 -0400

    Remove lightenColor

--- src/recipeScaler.js
+++ src/recipeScaler.js
@@ -100,21 +100,6 @@ module.exports = {
                 }
 
                 /**
-                 * Lightens a given hex color.
-                 * @param {string} color - The hex color to lighten.
-                 * @param {number} percent - The percentage to lighten by (0-100).
-                 * @returns {string} The lightened hex color.
-                 */
-                function lightenColor(color, percent) {
-                    const num = parseInt(color.replace("#",""), 16),
-                          amt = Math.round(2.55 * percent),
-                          R = (num >> 16) + amt,
-                          G = (num >> 8 & 0x00FF) + amt,
-                          B = (num & 0x0000FF) + amt;
-                    return "#" + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (G<255?G<1?0:G:255)*0x100 + (B<255?B<1?0:B:255)).toString(16).slice(1);
-                }
-
-                /**
                  * Converts a hex color to RGB values.
                  * @param {string} hex - The hex color to convert.
                  * @returns {Object} An object with r, g, and b properties.
